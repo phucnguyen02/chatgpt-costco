c5bf8545edce400326dd303f520d579dd726898b
phucnguyen02 <ndhp02@gmail.com>
2024-02-23 12:06:37 -0500

    Prompt changes

--- index.js
+++ index.js
@@ -34,47 +34,53 @@ app.post("/chat", async (req, res) => {
         model: "gpt-3.5-turbo",
         messages: [{"role": "user", "content": checkMessageType}],
     });
-
-    let messageType = parseInt(chatCompletion.choices[0].message.content.Message_Type);
+    console.log(chatCompletion.choices[0].message.content);
+    let messageType = parseInt(JSON.parse(chatCompletion.choices[0].message.content).Message_Type);
+    console.log(messageType);
     if(messageType == 4){
-        let errorResponse = {"Error": "Invalid Request"};
+        let errorResponse = {"error": "Invalid Request"};
         res.send(errorResponse);
     }
-
-    switch(messageType){
-        // Find nearest, cheapest gas station
-        case 1:
-            let address = body.address;
-            messageContent = `I am a user who is trying to refuel their car. My current location is ${address}. You are an advanced search \
-            engine, give me the cheapest Costco gas station that's closest to me and calculate the distance. Here is the list of Costco gas stations in Southern California: ${JSON.stringify(warehouseList)}. \
-            Please give me a response with a JSON format with the following fields: Station_Name, Address, City, State, Approximate_Distance, Regular_Gas, Premium_Gas.
-            Also, make sure Station_Name matches exactly the name that was provided in the list of stations.`
-            break;
-
-        // Find appropriate gas type
-        case 2:
-            let carBrand = chatCompletion.choices[0].message.content.Car_Brand;
-            messageContent = `I am a user who is trying to refuel their car in Orange County or a nearby location, and my current address is X
-            My current car brand is ${carBrand}. You are an advanced search engine, give me the appropriate gas type for my car and a
-            nearby Costco gas station so I can refuel. Here is the list of Costco gas stations in Southern California: ${JSON.stringify(warehouseList)}. \
-            Please give me a response with a JSON format with the following fields: Station_Name, Address, City, State, Recommended_Gas, Gas_Price.
-            Also, make sure Station_Name matches exactly the name that was provided in the list of stations. Gas_Price should be the corresponding price of the recommended gas type.`
-            break;
-
-        // Get gas trends, mermaidJS chart?
-        case 3:
-            messageContent = `I am a user in Orange County who would like to know about the trend of gas prices in this region. You are an advanced search \
-            engine, generate all of the gas prices for all the Costco gas stations within the past week. Here is the list of Costco gas stations in Southern California: ${JSON.stringify(warehouseList)}. \
-            Please give me a response with a JSON format with the following fields: Station_Name, Address, City, State, Gas_Trend.
-            Also, make sure Station_Name matches exactly the name that was provided in the list of stations.`
-            break;
+    else{
+        switch(messageType){
+            // Find nearest, cheapest gas station
+            case 1:
+                let address = body.address;
+                messageContent = `I am a user who is trying to refuel their car. My current location is ${address}. You are an advanced search \
+                engine, give me the cheapest Costco gas station that's closest to me and calculate the distance. Here is the list of Costco gas stations in Southern California: ${JSON.stringify(warehouseList)}. \
+                Please give me a response with a JSON format with the following fields: a JSON object with the field Warehouse_Info containing the following fields:
+                Station_Name, Address, City, State, Approximate_Distance, Regular_Gas, Premium_Gas, and the 2nd field named Prompt_Response being a user-friendly message containing all
+                of the necessary information about that warehouse using all the information from the Warehouse_Info field.  
+                Also, make sure Station_Name matches exactly the name that was provided in the list of stations.`
+                break;
+    
+            // Find appropriate gas type
+            case 2:
+                let carBrand = chatCompletion.choices[0].message.content.Car_Brand;
+                messageContent = `I am a user who is trying to refuel their car. My current address is ${address}. My current car brand is ${carBrand}. 
+                You are an advanced search engine, give me the appropriate gas type for my car and a
+                nearby Costco gas station so I can refuel. Here is the list of Costco gas stations in Southern California: ${JSON.stringify(warehouseList)}. \
+                Please give me a response with a JSON format with the following fields: a JSON object with the field Warehouse_Info containing the following fields: 
+                Station_Name, Address, City, State, Recommended_Gas, Gas_Price, and the 2nd field named Prompt_Response being a user-friendly message containing all
+                of the necessary information about that warehouse using all the information from the Warehouse_Info field.  
+                Also, make sure Station_Name matches exactly the name that was provided in the list of stations. Gas_Price should be the corresponding price of the recommended gas type.`
+                break;
+    
+            // Get gas trends, mermaidJS chart?
+            case 3:
+                messageContent = `I am a user in Orange County who would like to know about the trend of gas prices in this region. You are an advanced search \
+                engine, generate all of the gas prices for all the Costco gas stations within the past week. Here is the list of Costco gas stations in Southern California: ${JSON.stringify(warehouseList)}. \
+                Please give me a response with a JSON format with the following fields: Station_Name, Address, City, State, Gas_Trend.
+                Also, make sure Station_Name matches exactly the name that was provided in the list of stations.`
+                break;
+        }
+        chatCompletion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "user", "content": messageContent}],
+        });
+        console.log(chatCompletion.choices[0].message.content);
+        res.send(chatCompletion.choices[0].message.content);    
     }
-    chatCompletion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{"role": "user", "content": messageContent}],
-    });
-    console.log(chatCompletion.choices[0].message.content);
-    res.send(chatCompletion.choices[0].message.content);
 
 })
 
